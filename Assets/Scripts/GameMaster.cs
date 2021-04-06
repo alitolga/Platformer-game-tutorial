93db11cf51e70bc51380294f2eca081b99a668f1
alitolga <alitolga.dincer@gmail.com>
2021-04-06 09:20:17 +0300

    Initial commit of the tutorial that I followed

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
 
     public static GameMaster gm;
+
+    private static int _remainingLives = 3;
+    public static int RemainingLives
+    {
+        get { return _remainingLives; }
+    }
 
     private void Awake()
     {
@@ -22,10 +27,16 @@ public class GameMaster : MonoBehaviour
 
     public CameraShake cameraShake;
 
+
     private void Start()
     {
         if (cameraShake == null)
             Debug.LogError("No CameraShake reference in GameMaster");
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Game over");
     }
 
     public IEnumerator _RespawnPlayer()
@@ -42,7 +53,16 @@ public class GameMaster : MonoBehaviour
     public static void KillPlayer(Player player)
     {
         Destroy(player.gameObject);
-        gm.StartCoroutine(gm._RespawnPlayer());
+        _remainingLives -= 1;
+        if(_remainingLives <= 0)
+        {
+            // Game Over
+            gm.EndGame();
+        }
+        else
+        {
+            gm.StartCoroutine(gm._RespawnPlayer());
+        }
     }
     
     public static void KillEnemy(Enemy enemy)
